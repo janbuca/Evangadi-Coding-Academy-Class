28faef6482de0eb77726074af9453da020abd112
janbuca <janbuca333@gmail.com>
2024-03-12 00:57:26 -0700

    weeks 2 can be add

---
+++ Phase 3 ReactJs NodeJs MySql & ExpressJs/Week 2  Introduction to Node modules/p3-c3-Node-Modules-examples-updated-01-23-22/modules_example/main.js
@@ -0,0 +1,19 @@
+const abdu = require("./abebe");
+const kebe = require("./kebede");
+
+// ES6
+import AAvar1, { AAsomeFunction, sum } from "./abebe.js";
+
+// const kebe = require("./kebede");
+
+// console.log(global);
+// console.log("Test");
+// console.log(__filename);
+// console.log(__dirname);
+
+// console.log(abebe);
+
+abe.AAsomeFunction();
+kebe.AAsomeFunction();
+
+abe.sum(9, 10);
