c2857e35472454b81a5077828da802667ae25d92
janbuca <janbuca333@gmail.com>
2024-03-12 00:23:29 -0700

    Question 1 can be done

--- Phase 2 Learn coding with JavaScript/Week 1 Introduction to programming basics part I & 2/Question 1/js/script.js
+++ Phase 2 Learn coding with JavaScript/Week 1 Introduction to programming basics part I & 2/Question 1/js/script.js
@@ -1,7 +1,6 @@
-
 // How to test your JS 
-	// console.log("HELLO WORLD");
-	// alert("Selam");
+	console.log("HELLO WORLD");
+	window.alert("Selam");
 
 // Declaring Variables
 
@@ -60,6 +59,38 @@
 	}else {
 		console.log("Olla");
 	}
+// Question 1: Find out the answers for these by displaying it on the console. You will need to
+// compare the value on the left of the operator with the value on the right. Please note that
+// the answer for each question below should be either True or False.
+
+	console.log(24 > 3);
+	console.log(2 < "12");
+	console.log(0 == 2);
+	console.log(2.0 === 2);
+	console.log(2.0 == "2");
+	console.log(2 < "John");
+	console.log(2 > "John");
+	console.log("2" < "2");
+	console.log("2" > "12" && true);
+	console.log(1 == 1 || 3 == 2 || 3 == 7);
+	console.log(1 == 1 && 2 == 2 && 3 == 7);
+	console.log(1 == 1 || 2 == 2 && 3 == 7);
+	console.log(1 == true && 0 > true || "31" > "9" || 10 > 5 || !("2" == "two" || 1 == "1"));
+
+
+// Question 3:
+	// ● Write a simple script that adds 1 and 2 and displays the result on the console
+	// ● Use variables a, b & c
+	// ● Use "let" to declare the variables
+	// ● Use the formula c = a + b
+	// ● Display the value of c on console
+
+	
+	
+
+
+
+
 
 
 
