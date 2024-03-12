951ea0662f7765fe7716a78dc25d6fd3ae26daba
janbuca <janbuca333@gmail.com>
2024-03-12 05:35:08 -0700

    Something will be update

--- Phase 2 Learn coding with JavaScript/Week 2 Functions and conditional statements & Algorithmic Thinking/p2-c2-example/js/scripts.js
+++ Phase 2 Learn coding with JavaScript/Week 2 Functions and conditional statements & Algorithmic Thinking/p2-c2-example/js/scripts.js
@@ -4,182 +4,180 @@ a = 2876;
 b = 37654;
 c = a + b;
 d = a * b;
-// // console.log(d);
-e = (a + b) * (c / d);
-console.log(e);
+ // console.log(d);
+  e = (a + b) * (c / d);
+  console.log(e);
 
-// // String Operator (Concatenation)
-var a = "";
-var b = " Another Text";
-var c = a + b;
-console.log(c);
+ // String Operator (Concatenation)
+  var a = "";
+  var b = " Another Text";
+  var c = a + b;
+  console.log(c);
 
-// // Comparison Operators
-var a = "A";
-var b = 65;
-console.log(a == b);
+ // Comparison Operators
+  var a = "A";
+  var b = 65;
+  console.log(a == b);
 
 // ** Why is 5 less than A
 
-// // Logical Operators
+// Logical Operators
 console.log(1 == 1 || 2 == 2 || 3 == 7);
 
-// // Weak Typing
-var a = 5;
-var b = "T";
-a = true;
-console.log(a);
-
-var c = a + b;
-console.log(c);
-
-if (1 == 1) {
-  console.log("Working");
-}
-
-// // Array
-// // Declaring empty array
-let exampleArray = [];
-// // Adding values to an Array
-exampleArray = ["white", "black", "orange"];
-// // Accessing array values with index
-console.log(exampleArray);
-console.log(exampleArray[0]);
-console.log(exampleArray[1]);
-console.log(exampleArray[2]);
-
-// // Declaring/defining  a function
-
-function onn(id) {
-  console.log("We are on .. ");
-  console.log("The id is " + id);
-}
-// // Calling/using a function
-onn(345698);
-// onn(564321);
-
-// // Functions with arguments
-function myFunctionNameWithArgument(a) {
+// Weak Typing
+  var a = 5;
+  var b = "T";
+  a = true;
   console.log(a);
-}
-myFunctionNameWithArgument("function with one parameter");
 
-// // myFunctionNameWithArgument("Abebe beso bela");
-function myAdditionCalculator(a, b) {
-  let c;
-  c = a + b;
+  var c = a + b;
   console.log(c);
-}
-myAdditionCalculator(45, 99);
 
-// // Functions that return a value100));
-function myAdditionCalculator2(a, b) {
-  let c;
-  c = a + b;
-  var d = (a + b) / 2;
-  return [c, d];
-}
-// // How to use it
-// let a = myAdditionCalculator2(45, 99);
-// console.log(a);
-// // My average calculator
-function myAverageCalculator(a, b) {
-  var c = (a + b) / 2;
-  return c;
-}
-var ave = myAverageCalculator(40, 60);
-console.log(ave);
-// // Another way of writing the same function
-function myAverageCalculator2(a, b) {
-  var c = myAdditionCalculator2(a, b) / 2;
-  return c;
-}
-var ave = myAverageCalculator2(40, 60);
-console.log(ave);
+  if (1 == 1) {
+    console.log("Working");
+  }
+
+ // Array
+  // Declaring empty array
+      let exampleArray = [];
+  // Adding values to an Array
+      exampleArray = ["white", "black", "orange"];
+ // Accessing array values with index
+      console.log(exampleArray);
+      console.log(exampleArray[0]);
+      console.log(exampleArray[1]);
+      console.log(exampleArray[2]);
+
+// Declaring/defining  a function
+  function onn(id) {
+    console.log("We are on .. ");
+    console.log("The id is " + id);
+  }
+// Calling/using a function
+  onn(345698);
+  onn(564321);
+
+ // Functions with arguments
+    function myFunctionNameWithArgument(a) {
+      console.log(a);
+    }
+    myFunctionNameWithArgument("function with one parameter");
+
+// myFunctionNameWithArgument("Abebe beso bela");
+      function myAdditionCalculator(a, b) {
+        let c;
+        c = a + b;
+        console.log(c);
+      }
+      myAdditionCalculator(45, 99);
+
+// Functions that return a value100));
+    function myAdditionCalculator2(a, b) {
+      let c;
+      c = a + b;
+      var d = (a + b) / 2;
+      return [c, d];
+    }
+// How to use it
+    let a = myAdditionCalculator2(45, 99);
+    console.log(a);
+// My average calculator
+    function myAverageCalculator(a, b) {
+      var c = (a + b) / 2;
+      return c;
+    }
+  var ave = myAverageCalculator(40, 60);
+  console.log(ave);
+// Another way of writing the same function
+    function myAverageCalculator2(a, b) {
+      var c = myAdditionCalculator2(a, b) / 2;
+      return c;
+    }
+    var ave = myAverageCalculator2(40, 60);
+    console.log(ave);
 
 // Variable Scoping (var, let & const)
 
-
 // if condition or statement 
 
-function checkPassword(pass1, pass2) {
-  if (pass1 === pass2) {
-    console.log("The Password is Correct");    
-  }else{
-    console.log("The Password is Wrong");
-  }
-  
-}
+    function checkPassword(pass1, pass2) {
+      if (pass1 === pass2) {
+        console.log("The Password is Correct");    
+      }else{
+        console.log("The Password is Wrong");
+      }
+      
+    }
 
-checkPassword(5, 5);
-checkPassword(10, 15);
+    checkPassword(5, 5);
+    checkPassword(10, 15);
 
 // Terminal Operator
 
-function checkPasswordTerminal(test1, test2) {
-  test1 == test2
-  ? console.log("Correct password") 
-  : console.log("Wrong password");
-}
+    function checkPasswordTerminal(test1, test2) {
+      test1 == test2
+      ? console.log("Correct password") 
+      : console.log("Wrong password");
+    }
 
-checkPasswordTerminal(7,  7);
-checkPasswordTerminal(8,  7);
+    checkPasswordTerminal(7,  7);
+    checkPasswordTerminal(8,  7);
 
 // Swish Statement 
 
-function whattoDrink(time) {
+    function whattoDrink(time) {
 
-  let drink;
-  switch (time) {
-    case "morning":
-      console.log(drink = "Coffee!");
-      break;
+      let drink;
+      switch (time) {
+        case "morning":
+          console.log(drink = "Coffee!");
+          break;
+          
+        case "afternoon":
+          console.log(drink = "Tea!");
+          break;
+        case "night":
+            console.log(drink = "Milk!");
+            break;        
+        default:
+          console.log(drink ="water!");
+      }
+    }
+
+    whattoDrink("morning");
+    whattoDrink("afternoon");
+    whattoDrink("night");
+    whattoDrink("evening");
+
+
+
+
+    function stud(score) {
+      if (condition) {
       
-    case "afternoon":
-      console.log(drink = "Tea!");
-      break;
-    case "night":
-        console.log(drink = "Milk!");
-        break;        
-    default:
-      console.log(drink ="water!");
-  }
-}
-
-whattoDrink("morning");
-whattoDrink("afternoon");
-whattoDrink("night");
-whattoDrink("evening");
-
-
-
-
-function stud(score) {
-  if (condition) {
-  
-      return "The Studnt Values is Invalid score"
-  } else if (score >= 90) {
-      return "A+";
-  }else if (score >= 85) {
-      return "A";
-  }else if(score >= 80){
-      return "A-"
-  }else if (score >= 75) {
-      return "B+";
-  }else if(score >= 70){
-      return "B";
-  }else if(score >= 65){
-      return "B-";
-  }else if (score >= 60) {
-      return "C+";
-  }else if (score >= 55) {
-      return "C";
-  }else if (score >= 50) {
-      return "D";
-  }else{
-      return "F";
-      console.log("It is Failed blc the values is less than to '50'");
-  }
+          return "The Studnt Values is Invalid score"
+      } else if (score >= 90) {
+          return "A+";
+      }else if (score >= 85) {
+          return "A";
+      }else if(score >= 80){
+          return "A-"
+      }else if (score >= 75) {
+          return "B+";
+      }else if(score >= 70){
+          return "B";
+      }else if(score >= 65){
+          return "B-";
+      }else if (score >= 60) {
+          return "C+";
+      }else if (score >= 55) {
+          return "C";
+      }else if (score >= 50) {
+          return "D";
+      }else{
+          return "F";
+          console.log("It is Failed blc the values is less than to '50'");
+      }
 
   
 }
@@ -193,20 +191,20 @@ function stud(score) {
   // ○ First define the function
   // ○ Then call the function
 
-  function myFirst() {
-    console.log("Hello");    
-  }
-  myFirst();
+        function myFirst() {
+          console.log("Hello");    
+        }
+        myFirst();
 
 
   // Question 2
   // ● Define a function called mySecond that takes a parameter and prints the parameter on console
   // ○ Feel free to give any value as a parameter in your function
 
-  function mySecond(name) {
-    console.log(name);    
-  }
-  mySecond("Janbuca Teshome");
+        function mySecond(name) {
+          console.log(name);    
+        }
+        mySecond("Janbuca Teshome");
 
 
 
@@ -215,10 +213,10 @@ function stud(score) {
   // console. But, it uses mySecond function to print the parameter on the console
   // ○ Call mySecond function inside of myThird function with the parameter passed from myThird function
 
-  function myThird(x){
-    mySecond(x);
-  }
-  myThird("I am Janbuca__tesshaw");
+        function myThird(x){
+          mySecond(x);
+        }
+        myThird("I am Janbuca__tesshaw");
 
 
 
@@ -227,12 +225,12 @@ function stud(score) {
   // value of the array on the console. Remember, this question is here to explain the concept of
   // function delegation in JavaScript
 
-function myFourth(arr){
-   mySecond(arr[0]);
-   mySecond(arr[1]);
-   mySecond(arr[2]);
-}
-myFourth([5,"firew",false]);
+        function myFourth(arr){
+          mySecond(arr[0]);
+          mySecond(arr[1]);
+          mySecond(arr[2]);
+        }
+        myFourth([5,"firew",false]);
 
 
 
@@ -240,17 +238,17 @@ myFourth([5,"firew",false]);
   // ● Write a function named myFifth that takes an array with two numbers in it as a parameter
   // and prints the sum of the two numbers on console
 
-  function myFifth(a, b) {
-    let result = a + b; 
-    console.log(result);
-  } 
-  myFifth(6,9);
+        function myFifth(a, b) {
+          let result = a + b; 
+          console.log(result);
+        } 
+        myFifth(6,9);
 
-  function myFifth(a, b) {
-    return a + b;  
-  } 
-  console.log(myFifth(6,9));
-  console.log(myFifth(20,9));
+        function myFifth(a, b) {
+          return a + b;  
+        } 
+        console.log(myFifth(6,9));
+        console.log(myFifth(20,9));
  
 
 
@@ -258,81 +256,83 @@ myFourth([5,"firew",false]);
   // ● Write a function that takes an integer minutes and converts it to seconds.
   // ○ https://edabit.com/challenge/8q54MKnRrm89pSLmW
 
-  function convertMinToSecs(minutes) {
-    const secsPerMin = 60;
-    return minutes * secsPerMin;
-  } 
-  console.log(convertMinToSecs(1));
-  console.log(convertMinToSecs(2));
+        function convertMinToSecs(minutes) {
+          const secsPerMin = 60;
+          return minutes * secsPerMin;
+        } 
+        console.log(convertMinToSecs(1));
+        console.log(convertMinToSecs(2));
 
   // Question 7
   //     ● Create a function that takes a number as a parameter, increments the number by +1 and returns the result -->
   //     • Write a function called "multiplyArray" that takes in three arrays: arr1, arr2, and arr3.
 
-  function inc(num) {
-    console.log("The Given Number auto Increment by one.");
+      function inc(num) {
+        console.log("The Given Number auto Increment by one.");
 
-    if (typeof num !== "number") {
-        return "you enter the values is wrong plases changeing the values";
-    } else {
-        var x = num++;
-        return x;
+        if (typeof num !== "number") {
+            return "you enter the values is wrong plases changeing the values";
+        } else {
+            var x = num++;
+            return x;
+        }
     }
- }
- var num1 = inc(15);
- console.log(num1);
+    var num1 = inc(15);
+    console.log(num1);
 
 //  Question 8
 //    ● Write a function that takes the base and height of a triangle and returns its area
- function triArea(base,height){
-    let area=0.5*base*height;
-    return area;
-}
-console.log(triArea(10,5));
-
-function areaTri(width, height) {
-  console.log("Plase Calculate the area of the 'Triangle'")
-
-  if ((width <= 0|| height <=0) || (typeof  width !== "number" || typeof height !== "number")) {
-      return "You Entring is wrong a Number";
-  }else{
-      return ((width * height) / 2);
-  }
-}
-console.log(areaTri(5, 4));
+      function triArea(base,height){
+          let area=0.5*base*height;
+          return area;
+      }
+      console.log(triArea(10,5));
 
 
-var areaRec = function (width, height){
-  console.log("Plase Calculate the area of the Rectangle is")
-  if ((width < 0|| height <0) || (typeof  width !== "number" || typeof height !== "number")) {
-      return "You Entring is wrong a Number";
-  }else{
-      return ((width * height));
-  }                    
-}
-console.log(areaRec(5, 5));
+
+      function areaTri(width, height) {
+        console.log("Plase Calculate the area of the 'Triangle'")
+
+        if ((width <= 0|| height <=0) || (typeof  width !== "number" || typeof height !== "number")) {
+            return "You Entring is wrong a Number";
+        }else{
+            return ((width * height) / 2);
+        }
+      }
+      console.log(areaTri(5, 4));
+
+
+      var areaRec = function (width, height){
+        console.log("Plase Calculate the area of the Rectangle is")
+        if ((width < 0|| height <0) || (typeof  width !== "number" || typeof height !== "number")) {
+            return "You Entring is wrong a Number";
+        }else{
+            return ((width * height));
+        }                    
+      }
+      console.log(areaRec(5, 5));
 
 // Question 9
 //    ● Create a function that returns the total number of legs of all the animals.
 //     In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals.
 //     The farmer breeds three species (chickens = 2 legs, cows =, 4 legs, pigs = 4 legs). Remember:
 //     the farmer wants to know the total number of legs and not the total number of animals.
-    function getTotalLegs() {
-        var chickenlegs = 2;
-        var cowlegs = 4;
-        var piglegs = 4 ;
-        var totallegs = chickenlegs + cowlegs + piglegs;
-        
-        return totallegs;
-    };
-    console.log(getTotalLegs());
+        function getTotalLegs() {
+            var chickenlegs = 2;
+            var cowlegs = 4;
+            var piglegs = 4 ;
+            var totallegs = chickenlegs + cowlegs + piglegs;
+            
+            return totallegs;
+        };
+        console.log(getTotalLegs());
 
 
-    console.log("Calculate the total number of legs of 3  animals");
-                function animalsLegs(chickens, cows, pigs) {
-                    if (chickens <= 0 ||cows <=0 || chickens <=0) {
-                        return "It Only Enter a postif "
-                    }else if (typeof chickens !== "number" ||typeof cows !== "number" ||typeof chickens !== "number") {
+        console.log("Calculate the total number of legs of 3  animals");
+            function animalsLegs(chickens, cows, pigs) {
+                if (chickens <= 0 ||cows <=0 || chickens <=0) {
+                    return "It Only Enter a postif "
+                  }else if (typeof chickens !== "number" ||typeof cows !== "number" ||typeof chickens !== "number") {
                         
                     } else {
                         return ((chickens * 2) + (cows* 4) + (chickens * 4));
@@ -344,17 +344,17 @@ console.log(areaRec(5, 5));
   
   // Question 10 (not from edabit.com)
   // ● Create a function that takes an array containing only TWO numbers as a parameter and returns a value that is 3 times the first element of the array.
-  function arrayTwo(n) {
-    return [n[0] * 3, n[1]];
-}
-var arr = ([5, 50]);
-console.log(arrayTwo(arr));
+          function arrayTwo(n) {
+            return [n[0] * 3, n[1]];
+          }
+          var arr = ([5, 50]);
+          console.log(arrayTwo(arr));
 
 /*
 <!-- Questions on Conditional statements and - practice exercise -->
 
-            <!-- Question 11
-            ● Create a function that returns true when num1 is equal to num2; otherwise return false. -->
+    <!-- Question 11
+      ● Create a function that returns true when num1 is equal to num2; otherwise return false. -->
 */
     function compareNumbers(num1, num2){
       if (num1 == num2){
@@ -367,22 +367,22 @@ console.log(arrayTwo(arr));
 
 
 
-    let chek = (num1, num2)=> {
-      if(typeof(num1) !== "number" || typeof(num2) !== "number"){
-       return "The Entring values is String plases change to a Number";
-   }else if(num1 === num2){
-       return "The OutPut value is 'Ture' b/c it is 'Equal'";
-   }else{
-       return "The output values is 'False' b/c it not 'Equal'";
-   }
-   return num1 === num2? true:false;
-   return num1 === num2;
-}
-console.log(chek(5,5));
-console.log(chek(5,10));
-console.log(chek(5,"5"));
-console.log(chek(5,"Janbuca"));
-console.log(chek(-5,-5));
+            let chek = (num1, num2)=> {
+              if(typeof(num1) !== "number" || typeof(num2) !== "number"){
+              return "The Entring values is String plases change to a Number";
+          }else if(num1 === num2){
+              return "The OutPut value is 'Ture' b/c it is 'Equal'";
+          }else{
+              return "The output values is 'False' b/c it not 'Equal'";
+          }
+          return num1 === num2? true:false;
+          return num1 === num2;
+        }
+        console.log(chek(5,5));
+        console.log(chek(5,10));
+        console.log(chek(5,"5"));
+        console.log(chek(5,"Janbuca"));
+        console.log(chek(-5,-5));
 
 /*
 < Question 12
@@ -420,19 +420,19 @@ console.log(chek(-5,-5));
             ● Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers -->
 
    */
-  function chek(x,y){
-      let res = [];
-      res.push(`Value of x is ${x} and value of y is ${y}`);
-      res.push(checkTypeOf(x));
-      res.push(checkTypeOf(y))
-      return res;
-  };
+      function chek(x,y){
+          let res = [];
+          res.push(`Value of x is ${x} and value of y is ${y}`);
+          res.push(checkTypeOf(x));
+          res.push(checkTypeOf(y))
+          return res;
+      };
 
-  console.log(chek('Hello',123));
+        console.log(chek('Hello',123));
 
 
 
-  console.log(evOdd(14));
+          console.log(evOdd(14));
                 function evOdd(x) {
                     if ((x % 2) == 0) {
                         return 'The Given Number is "Even"';
@@ -451,26 +451,26 @@ console.log(chek(-5,-5));
                 ○ “Grade B” when score is between 80 and 89 (both 80 and 89 included)
                 ○ “Grade C” for any score below 79 -->
  */
-function gradeScore(score){
-    //Checking the conditions one by one in order from highest to lowest
-    if(isNaN(score)){
-       return "Invalid Score";
-     } else if(score < 0){
-         return "Invalid Score";
-     } else if(score > 100){
-         return "Invalid Score";
-     } else if(score >= 90 && score <= 100){
-          return "Grade A";
-     } else if(score >= 80){
-           return "Grade B";
-     } else {
-            return "Grade C";
-      } 
-}
-console.log(gradeScore("abc"));//Expected Output: Invalid Score 
-console.log(gradeScore(-50));//Expected Output: Invalid Score
-console.log(gradeScore(105));//Expected Output: Invalid Score
-console.log(gradeScore(85));//Expected Output: Grade B 
+          function gradeScore(score){
+              //Checking the conditions one by one in order from highest to lowest
+              if(isNaN(score)){
+                return "Invalid Score";
+              } else if(score < 0){
+                  return "Invalid Score";
+              } else if(score > 100){
+                  return "Invalid Score";
+              } else if(score >= 90 && score <= 100){
+                    return "Grade A";
+              } else if(score >= 80){
+                    return "Grade B";
+              } else {
+                      return "Grade C";
+                } 
+          }
+      console.log(gradeScore("abc"));//Expected Output: Invalid Score 
+      console.log(gradeScore(-50));//Expected Output: Invalid Score
+      console.log(gradeScore(105));//Expected Output: Invalid Score
+      console.log(gradeScore(85));//Expected Output: Grade B 
 
 
 
