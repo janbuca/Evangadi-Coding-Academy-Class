fcca1c880e04c3449a55bc9c6ea922208fede20a
janbuca <janbuca333@gmail.com>
2024-03-16 05:58:31 -0700

    Question two can be done

--- Phase 2 Learn coding with JavaScript/Week 3 Decision and loops & OOP/JS Loop and Object/js/script.js
+++ Phase 2 Learn coding with JavaScript/Week 3 Decision and loops & OOP/JS Loop and Object/js/script.js
@@ -6,4 +6,23 @@ function integer() {
         console.log(i);
      }
 }
-    integer(); 
+    // integer(); 
+
+
+    /* Question 2
+    ● writing the next 5 numbers in the console. Note: each output should be displayed on a new line. a function that takes a single number as an argument and p
+         ○ Test case: If you give 7 to the function, output should look like this:
+                    8
+                    9
+                    10
+                    11
+                    12 
+    */
+
+    function print(num) {
+        for(let i =num+1; i<=num+5; i++){
+            console.log(i);
+            }
+        }
+    print(7);
+                    
