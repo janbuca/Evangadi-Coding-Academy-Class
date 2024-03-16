fd6b613a32645ad45677a4feb40aff8301b02f78
janbuca <janbuca333@gmail.com>
2024-03-16 05:56:03 -0700

    Question one can be done

---
+++ Phase 2 Learn coding with JavaScript/Week 3 Decision and loops & OOP/JS Loop and Object/js/script.js
@@ -0,0 +1,9 @@
+// Question 1
+//      ● Write a function that prints the first 10 integers on the console starting from the number 1 using the JavaScript for loop. -->
+
+function integer() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+     }
+}
+    integer(); 
