6a7b05ed0eeac4a0e2e542d386e832866bdff0ed
janbuca <janbuca333@gmail.com>
2024-03-19 11:35:00 -0700

    All of the selecter can be done

---
+++ Phase 2 Learn coding with JavaScript/Week 5 JavaScript DOM manipulation & Events/Dom/js/script.js
@@ -0,0 +1,37 @@
+let firstList = document.getElementById("one");
+// console.log(firstList);
+
+// console.log(document.querySelector("#two"));
+// console.log(document.querySelector(".red"));
+// console.log(document.querySelectorAll("h1"));
+
+
+// // Select multiple element
+//// 1 is getElementByClassName() method
+// let redElement = document.getElementsByClassName("red");
+// console.log(redElement);
+// console.log(redElement[0]); //accesing the element by index 0
+// console.log(redElement.length); //how many elements are there in this class?
+// console.log(redElement.item(2)); //accessing an element using item() method and passing the index as parameter
+
+
+// // 2 getElementByTagName()  Method
+// let tagElement = document.getElementsByTagName("h1")
+// console.log(tagElement);
+// console.log(tagElement.length);
+// console.log(tagElement[4]);
+
+// 3 querySelector() Method
+
+// let queryElement = document.querySelectorAll( "h1" );
+// console.log(queryElement);
+// console.log(queryElement.length);
+// console.log(queryElement[0]);
+// console.log(document.querySelectorAll(".red"));
+
+console.log(document);
+// DOM node is represent everything
+console.log(document.body.childNodes);
+
+// DOM element represent - html element
+console.log(document.body.children);
